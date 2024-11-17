d2183ab03291a82b9fafa23c81b4648c21a4907a
maib07001 <maib07001@E-PW05C37V>
2024-11-17 16:40:00 +0100

    Add project files.

---
+++ Uppgift.2.4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace Uppgift_5._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("har gått ut gymmnasiet");
+            int ålder = int.Parse(Console.ReadLine());
+
+            if (ålder <22)
+
+            {
+                Console.WriteLine("vi vill gärna anställa dig");
+            }
+              else if (ålder >25)
+            {
+                Console.WriteLine("vi letar tyvärr efter annan personal just nu");
+
+            }
+            else
+            {
+                Console.WriteLine("vi letar tyvärr efter annan personal just nu");
+            }
+        }
+    }
+}
